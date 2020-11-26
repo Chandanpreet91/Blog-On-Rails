15cb0f428bed07fd952f6e17f7b01de6db87f65d
Chandanpreet91 <kaurchandanpreet91@gmail.com>
2020-11-26 11:59:27 -0800

    add bcrypt for hash passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :comments, dependent: :nullify
     has_many :posts, dependent: :nullify
+    has_secure_password
 
     VALID_EMAIL_REGEX = /\A([\w+\-],?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX
