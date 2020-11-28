255dd6e3e4648848625a19d3482de7cdbdb0afbb
Chandanpreet91 <kaurchandanpreet91@gmail.com>
2020-11-27 18:19:22 -0800

    added session controller and helper methods in application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-get "/" , {to: 'posts#index'}
-resources :posts do 
-    resources :comments, only: [:create, :destroy]
-end
+   get "/" , {to: 'posts#index' , as: :root}
+   resources :posts do 
+   resources :comments, only: [:create, :destroy]
+   end
 
+   resources :users, only: [:new, :create]
 
+   resource :sessions, only: [:new, :destroy, :create]
 
 end 
